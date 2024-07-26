a472b1faf2ad08f0d3b2ef3a8d48699feac45a4b
Vidra22 <145906805+Vidra22@users.noreply.github.com>
2024-07-26 10:47:53 +0300

    Update play.js

--- play.js
+++ play.js
@@ -1,15 +1,32 @@
-let correctCardIndex = Math.floor(Math.random() * 7);
+let correctCardIndex = Math.floor(Math.random() * 4);
+let hintUsed = false;
 
 function checkCard(card) {
     const cards = document.querySelectorAll('.card');
     const wrongImage = document.getElementById('wrong-image');
 
     if (Array.from(cards).indexOf(card) === correctCardIndex) {
-        alert("Вітаю! Ви вижили!");
+        alert("Вітаю! Ви Вижили!");
     } else {
         wrongImage.classList.remove('hidden');
         setTimeout(() => {
             wrongImage.classList.add('hidden');
         }, 1500); // Зображення зникає через 1 секунду
     }
+}
+
+function useHint() {
+    if (hintUsed) return;
+
+    const cards = document.querySelectorAll('.card');
+    let incorrectCardIndex;
+
+    do {
+        incorrectCardIndex = Math.floor(Math.random() * 4);
+    } while (incorrectCardIndex === correctCardIndex);
+
+    cards[incorrectCardIndex].style.backgroundColor = '#ff6666'; // Позначаємо неправильну карту червоним
+
+    hintUsed = true;
+    document.getElementById('hint-button').disabled = true;
 }
